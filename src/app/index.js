80c172461410c9a914ae065eb2efb1fd3cff7be4
aykay88 <andreas.kaiser@andrena.de>
2017-02-07 22:55:58 +0100

    rest

--- src/app/index.js
+++ src/app/index.js
@@ -1,67 +1,53 @@
 var React = require('react');
 
 var ReactDOM = require('react-dom');
+require('./css/index.css')
 
+// Module require
 
-/*
-+++++++++++++++++++props beispiel++++++++++++
+var TodoItem = require('./todoItem')
 
 //Create component
 var TodoComponent = React.createClass({
+    getInitialState: () => {
+        return {
+            todos: [
+                'wash up', 'eat some cheese', 'take a nap', 'pick some flowers'
+            ],
+            age: 30
+        }
+    },
     render: function() {
+
+        var todos = this.state.todos;
+        todos = todos.map(function(item, index) {
+            // spaltet state todos liste auf und erzeugt li einträge
+
+            return (<TodoItem item={item} key={index} onDelete={this.onDelete}/>);
+
+        }.bind(this)); // TodoItemComponent weiß sonst nicht was this ist
+
         return (
-          <div>
-          <h1>jsx behind the scenes</h1>
-          <p> 2. tag</p>
-          <p> {this.props.mssg}</p>
-<p> <strong>Cheese name:</strong>{this.props.cheese.name}</p>
-<p> <strong>Cheese smell:</strong>{this.props.cheese.smellFactor}</p>
-<p> <strong>Cheese price:</strong>{this.props.cheese.price}</p>
-          </div>
+            <div id="todo-list">
+                <p>
+                    The busiest people have the most leisure....</p>
+
+                <ul>{todos}</ul>
+            </div>
         );
 
+    }, // render
+
+    //Custom functions
+    onDelete: function(item) {
+        var updatedTodos = this.state.todos.filter(function(val, index) {
+            return item !== val;
+        });
+        this.setState({todos: updatedTodos});
     }
 });
 
-var myCheese = {name:'Camonbert', smellFactor:' Extreme pong', price:'2.50' }
 //put component into html page
 
-ReactDOM.render(<TodoComponent mssg="i like cheese" cheese={myCheese}/>,document.getElementById('todo-wrapper'));
-*/
-
-
-//Create component
-var TodoComponent = React.createClass({
-getInitialState: ()=> {
-  return {
-    todos: ['wash up','eat some cheese','take a nap'],
-    age:30
-  }
-},
-    render: function() {
-      /*
-      +++++Change+++++++
-      var ager = setTimeout( function(){
-        this.setState({
-          age:35
-        })
-      }.bind(this),5000);
-      */
-        return (
-          <div id="todo-list">
-            <p> The busiest people have the most leisure....</p>
-            <p> {this.state.age}</p>
-            <ul>
-              <li>{this.state.todos[0]}</li>
-              <li>{this.state.todos[1]}</li>
-              <li>{this.state.todos[2]}</li>
-            </ul>
-          </div>
-        );
-
-    } // render
-});
-
-//put component into html page
-
-ReactDOM.render(<TodoComponent />,document.getElementById('todo-wrapper'));
+ReactDOM.render(
+    <TodoComponent/>, document.getElementById('todo-wrapper'));
